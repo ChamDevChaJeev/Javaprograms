38cd944fd8f0be8b9611d4d2b127c35750424829
ChamDevChaJeev <devika.shree4@gmail.com>
2021-09-11 17:02:53 +0530

    create class and object

---
+++ DevsJava.java
@@ -0,0 +1,14 @@
+//Java program how to define a class
+//create variable
+//create an object or instance
+class student{
+int id=1;//instant variable declared inside class but outside main
+String name="Jeeth";
+//cewate main method
+//public static void main(String[] args){
+public static void main(String args[]){  
+//create object or instance
+student s=new student();
+System.out.println("Student name: "+s.name+" Student id: "+s.id);
+}
+}
