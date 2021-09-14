f979f39e0a3dbbbf1d56ab0d71a5793898b7d000
ChamDevChaJeev <devika.shree4@gmail.com>
2021-09-14 13:14:14 +0530

    String builder,String buffer, static keyword and this keyword implemnted

--- mystrings/OOPSimplementation.java
+++ mystrings/OOPSimplementation.java
@@ -2,11 +2,17 @@
 package mystrings;
 class Oops extends StringLearn {
 //through extend keyword we could inherit StringLearn class into Oops class
+static{System.out.println("i am a static block inside Oops class ");}
 public static void main(String args[]){
 //create instance of Oops class
 Oops oop=new Oops();
 oop.tocreatestring();
 oop.toconcate();
-}
+
+//to use parameterised constructor os StringLearn class
+StringLearn sl=new StringLearn("Sachin","Sachin","Dravid");
+sl.stringComparison();
+StringBufferExample();//since StringBufferExample(); method is made static we need not create instance of the class
+StringBuilderExample();}
 }
 
--- mystrings/StingLearn.java
+++ mystrings/StingLearn.java
@@ -1,9 +1,20 @@
 //this program concentrates on string class and its method
 //creation of string
+//String concatination
+//string buffer
 
 package mystrings;
 
 class StringLearn{
+String st1,st2,st3;
+//create constructor
+//whenever object of the class is created constructor block is invoked 
+StringLearn(){System.out.println("I am StringLearn class");}
+//create a parameterised constructor
+StringLearn(String st1,String st2,String st3){
+this.st1=st1;//when current class instant variable and local variables are same "this" keyword is used.
+this.st2=st2;
+this.st3=st3;}
 void tocreatestring(){
 //string can be created in 3 ways
 String s1="Welcome";
@@ -18,4 +29,24 @@ String s2="Dravid";
 String s3=s1+s2;
 System.out.println("String Concatination using +: Rahul+Dravid= " + s3);
 System.out.println("String Concatenation using .concate()= "+s1.concat(s2));
-}}
+}
+void stringComparison(){
+System.out.println("Comparison of String using '.equals': Sachin.equals(sachin)= "+st1.equals(st2));
+System.out.println("Comparison of String using '.equals': Sachin.equals(Dravid)= "+st1.equals(st3));
+System.out.println("Comparison of String using 'ComparedTo': Sachin.ComparedTo(Sachin)= "+st1.compareTo(st2));
+System.out.println("Comparison of String using 'ComparedTo': Sachin.ComparedTo(Dravid)= "+st1.compareTo(st3));
+
+}
+//String buffer is used to create mutable string
+//this method belongs to class memory 
+static void StringBufferExample(){
+StringBuffer sb=new StringBuffer("Sachin");
+sb.append("Tendulkar");
+System.out.println("StringBuffer is used to create mutable string(example) : "+ sb);}
+
+static void StringBuilderExample(){
+StringBuilder sB=new StringBuilder("Hello");
+sB.insert(1,"Java");
+System.out.println("StringBuilder also creats mutable string more efficient than StringBuffer : "+ sB);}
+}
+
